8d1aa4081c4141dd89af3d37442bf637b698c0e3
totvstec <totvstec@tec-macmini-qt.tec01.local>
2017-06-16 17:02:37 -0300

    Lógica para cópia dos recursos do CloudBridge para iOS

--- install.js
+++ install.js
@@ -54,11 +54,11 @@ class InstallTask {
 			.then(function() {
 				return self.copySources();
 			});
-			// .then(function() {
-			// 	var restoreTask = require('./restore');
+		// .then(function() {
+		// 	var restoreTask = require('./restore');
 
-			// 	return restoreTask.run(self.cli, self.projectDir, self.projectData);
-			// });
+		// 	return restoreTask.run(self.cli, self.projectDir, self.projectData);
+		// });
 	}
 
 	cleanSources() {
@@ -72,7 +72,63 @@ class InstallTask {
 	}
 
 	copySources() {
-		// TODO
+		var srcPath = path.join(__dirname, "src"),
+			binPath = path.join(__dirname, "libs"),
+			assetsPath = path.join(__dirname, "assets"),
+			projPath = path.join(__dirname, "prj"),
+			project = require(path.join(this.projectDir, 'package.json')),
+			projectDisplayName = project.displayName || "HelloCordova",
+			targetPath = path.join("platforms", "ios"),
+			targetSrcPath = path.join(targetPath, projectDisplayName),
+			targetBinPath = targetPath,
+			targetAssetsPath = path.join(targetPath, projectDisplayName),
+			targetProjPath = path.join(targetPath, projectDisplayName + ".xcodeproj");
+
+		//Sources
+		var fsources = shelljs.ls(path.join(srcPath, "/*"));
+		for (var i = 0; i < fsources.length; i++) {
+			var targetFile = path.join(targetSrcPath, path.basename(fsources[i]));
+			shelljs.cp("-f", fsources[i], targetFile);
+		}
+		//Remove default main.m
+		var srcMainStdFile = path.join(targetSrcPath, "main.m");
+		if (shelljs.test('-e', srcMainStdFile)) {
+			shelljs.rm("-f", srcMainStdFile);
+		}
+
+		//Binario
+		var fbin = shelljs.ls("-d", path.join(binPath, "/*"));
+		for (var i = 0; i < fbin.length; i++) {
+			shelljs.cp("-rf", fbin[i], targetBinPath);
+		}
+		//Unzip Binario
+		var zipBinLibFile = path.join(targetPath, "libappserver.zip");
+		if (shelljs.test('-e', zipBinLibFile)) {
+			if (shelljs.exec('unzip ' + zipBinLibFile + ' -d ' + targetPath).code !== 0) {
+				shelljs.echo('Error: unzip failed');
+			}
+			else {
+				shelljs.rm("-rf", zipBinLibFile);
+			}
+		}
+
+		//Assets
+		var fassets = shelljs.ls(path.join(assetsPath, "*"));
+		for (var i = 0; i < fassets.length; i++) {
+			shelljs.cp("-rf", fassets[i], targetAssetsPath);
+		}
+
+		//Project
+		var fprojs = shelljs.ls(path.join(projPath, "/*"));
+		for (var i = 0; i < fprojs.length; i++) {
+			shelljs.cp("-rf", fprojs[i], targetProjPath);
+		}
+		var projFile = path.join(targetProjPath, "project.pbxproj");
+		if (shelljs.test('-e', projFile)) {
+			var content = fs.readFileSync(projFile, { encoding: 'utf8' });
+			content = content.replace(/<%= displayName %>/igm, projectDisplayName);
+			fs.writeFileSync(projFile, content);
+		}
 	}
 
 	checkForExistingFiles() {
---
+++ src/appserver.h
@@ -0,0 +1,7 @@
+#ifdef __cpluplus
+extern "C" {
+#endif
+int runServer( int argc, char** argv );
+#ifdef __cpluplus
+}
+#endif
